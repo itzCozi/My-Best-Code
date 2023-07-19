cb9909d7459bcfa2854d00a142b954a9bdb419a1
Cooper <Cooperransom08@gmail.com>
2023-07-19 01:02:02 -0400

    Update helper

--- The-Code/helper.py
+++ The-Code/helper.py
@@ -8,6 +8,7 @@ try:
   import time
   import string
   import random
+  import threading as THC
   from cryptography.fernet import Fernet
 except Exception as e:
   print(f'ERROR: An error occurred when importing dependencies. \n{e}\n')
@@ -23,6 +24,66 @@ class vars:  # Variable class/container
 
 
 class functions:
+
+  def loadingBar(duration, endMsg=None, symbol=None):
+    if not isinstance(duration, int):
+      raise Exception('duration variable must be a integer')
+    if endMsg != None:
+      if not isinstance(endMsg, str):
+        raise Exception('endMsg variable must be a string')
+    else:
+      endMsg = 'Operation Finished.'
+    if symbol != None:
+      if not isinstance(symbol, str):
+        raise Exception('symbol variable must be a string')
+    else:
+      symbol = '#'
+    cycle_time = round(duration / 100, 2)
+    bar = [
+      '[', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+      ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+      ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+      ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+      ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+      ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+      ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+      ' ', ' ', ' ', ']'
+    ]
+
+    def innerBar():
+      ticker = 0
+      clear = '\x1b[2K'
+      for cycle in range(0, 100):
+        ticker += 1
+        bar[ticker] = symbol
+        percent = f' {ticker}%\r'
+        print(''.join(bar), end=percent)
+        time.sleep(cycle_time)
+      print(clear, end='\r')
+      print(endMsg)
+
+    thread = THC.Thread(innerBar())
+    thread.start()
+    thread.join()
+
+  def stall(duration):
+    if not isinstance(duration, int):
+      raise Exception('duration variable must be a integer')
+
+    def innerStall(arg):
+      for cycle in range(arg):
+        print('|', end='\r')
+        time.sleep(0.15)
+        print('/', end='\r')
+        time.sleep(0.15)
+        print('-', end='\r')
+        time.sleep(0.15)
+        print('\\', end='\r')
+        time.sleep(0.15)
+
+    thread = THC.Thread(innerStall(duration))
+    thread.start()
+    thread.join()
 
   def genID():
     # Creates a unique ID
